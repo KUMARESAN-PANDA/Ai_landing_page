18e74f9b45543ac550f98e29ca4f39c1ec7f656e
KUMARESAN <164299143+KUMARESAN-PANDA@users.noreply.github.com>
2024-12-14 18:34:33 +0530

    first commit

---
+++ src/assets/xora_public/social.js
@@ -0,0 +1,24 @@
+// src/assets/xora_public/socials.js
+import s1 from "../assets/xora_public/public/images/socials/discord.svg"
+
+const socials = [
+    {
+      id: 1,
+      url: "https://facebook.com",
+      icon: s1,  // Adjusted to public path
+      title: "Facebook",
+    },
+    {
+      id: 2,
+      url: "https://twitter.com",
+      icon: s1,  // Adjusted to public path
+      title: "Twitter",
+    },
+    {
+      id: 3,
+      url: s1,
+      icon: "../assets/xora_public/public/images/socials/discord.svg",  // Adjusted to public path
+      title: "Instagram",
+    },
+  ];
+  
